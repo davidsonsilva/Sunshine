012614695328cb281b7d2fa76bb160a9cd4a8afc
davidsonsilva <davidson.f.silva@gmail.com>
2018-07-01 20:03:07 -0300

    Exercise Dysolay day forecast completes

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,23 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private TextView mDetail_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        mDetail_text = (TextView) findViewById(R.id.detail_text);
+        Intent startedIntent = getIntent();
+        if(startedIntent != null && startedIntent.hasExtra(Intent.EXTRA_TEXT)) {
+            mDetail_text.setText(startedIntent.getStringExtra(Intent.EXTRA_TEXT));
+        }
     }
 }
